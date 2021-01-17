35ed3d2c5620dd9d72dd20e121833a3fbe3e030a
Cinvin Tom <65575529+CINVINTOM@users.noreply.github.com>
2021-01-17 23:23:35 +0530

    Update Labc2Q2.java

--- Labc2Q2.java
+++ Labc2Q2.java
@@ -1,8 +1,14 @@
+/*2.Define a class Queue for representing a queue data 
+structure. The class must define a default 
+constructor, a parameterized constructor and 
+functions for en queue, de-queue and display 
+operations. Write a Java program to implement this.*/
 
+import java.util.Scanner;
 
 public class Queue 
        {
-	public static final int DEFAULT_SIZE = 5;
+	public static final int DEFAULT_SIZE = 4;
 	private Object data[];
 	private int index;
 
@@ -61,3 +67,63 @@ public class Queue
 class Labc2Q2
 {
         Public static void main(Strings arg[])
+        {
+               Scanner s = new Scanner(System.in);
+               int SIZE=4;
+               System.out.println("Program to impliment Queue") ;
+               System.out.println("Default size of Queue is 4") ;
+               public void menu(){
+               System.out.println("MENU") ;
+               System.out.println("\n 1 for Change size of Queue") ;
+               System.out.println("2 for enqueue") ;
+               System.out.println("3 for dequeue") ;
+               System.out.println("4 for Display Queue elements") ;
+               System.out.println(5 for EXIT") ;
+               System.out.println("Enter your choice (1/2/3/4/5) : ") ;
+               int c = s.nextInt();
+               Queue queue = new Queue();
+               if(c==1)
+               {
+                  
+                   System.out.println("ENTER THE SIZE  :") ;
+                   int SIZE = s.nextInt();
+                   Queue queue = new Queue(SIZE);
+                   this.menu();
+                }
+               else if(c==2)
+               {
+                  
+                   System.out.println("ENTER THE element to be inserted  :") ;
+                   int e = s.nextInt();
+                  
+                   queue.enqueue(e);
+
+                   this.menu();
+                }
+              else if(c==3)
+               {
+                  
+                   
+                   
+                   System.out.println(queue.dequeue() + "Is removed from queue");
+                   this.menu();
+                }
+               else if(c==4)
+               {
+                  
+                   
+                   queue.display());
+                   this.menu();
+                }
+               else if(c==5)
+               {
+                  
+                   System.out.println("FINISHED") ;               
+               }
+               else
+               {
+                  
+                   System.out.println("Not proper input") ;               
+               }
+        }        
+}
