d5f82f77c5e9ec9ee85c4fcf58b2bfa89e6e7559
Cinvin Tom <65575529+CINVINTOM@users.noreply.github.com>
2021-01-17 16:10:30 +0530

    Update Labc2Q5.java

--- Labc2Q5.java
+++ Labc2Q5.java
@@ -1,3 +1,10 @@
+/*5.Create a class with a method that prints This is parent class" and its subclass with another method that prints "This is child class".
+ Now, create an object for each of the class and call
+1 - method of parent class by object of parent class
+2- method. child class by object of child class 
+3 - method of parent class by object of child class*/
+
+
 class Labc2Q5
 {
     public static void main(String arg[])
