165a62196630a92b809a90688d9292dace12cbea
Cinvin Tom <65575529+CINVINTOM@users.noreply.github.com>
2021-01-17 23:51:46 +0530

    Update Labc2Q5.java

--- Labc2Q5.java
+++ Labc2Q5.java
@@ -1,4 +1,5 @@
-/*5.Create a class with a method that prints This is parent class" and its subclass with another method that prints "This is child class".
+/*5.Create a class with a method that prints This is parent class" and its subclass with
+another method that prints "This is child class".
  Now, create an object for each of the class and call
 1 - method of parent class by object of parent class
 2- method. child class by object of child class 
