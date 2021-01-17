fff7ec84e5086e526dd2cb6059b4bf9803110da0
Cinvin Tom <65575529+CINVINTOM@users.noreply.github.com>
2021-01-17 23:49:47 +0530

    Update Labc2Q4.java

--- Labc2Q4.java
+++ Labc2Q4.java
@@ -1,6 +1,9 @@
-/*4.Write a program to print the area of a rectangle by creating a class named 'Area' taking the values of its
-length and breadth as parameters of its constructor and having a method named 'returnArea' which returns
-the area of the rectangle. Length and breadth of rectangle are entered through keyboard.*/
+/*4.Write a program to print the area of a rectangle by creating 
+a class named 'Area' taking the values of its
+length and breadth as parameters of its constructor and having 
+a method named 'returnArea' which returns
+the area of the rectangle. Length and breadth of rectangle are 
+entered through keyboard.*/
 
 import java.util.Scanner;
 
